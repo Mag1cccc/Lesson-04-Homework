3aa02faf8aeb0220b30435da247f139752893f1b
amiko morgoshia <morgoshiaaamiko@gmail.com>
2026-05-22 00:00:45 +0400

    Add basic calculator with min-max check and zero-divison check

--- Homework_5/Program.cs
+++ Homework_5/Program.cs
@@ -5,20 +5,33 @@
         static void Main(string[] args)
 
         {
-            Console.Write("Enter a number: ");
-            string userInput = Console.ReadLine();
+            Console.Write("Enter first number: ");
+            string? firstInput = Console.ReadLine();
+            Console.Write("Enter second number: ");
+            string? secondInput = Console.ReadLine();
 
-            if (int.TryParse(userInput, out int number))
+            if (int.TryParse(firstInput, out int firstNumber) && int.TryParse(secondInput, out int secondNumber))
             {
-                if (number % 5 == 0)
-                {
-                    Console.WriteLine("Yes");
+                int sum = firstNumber + secondNumber;
+                int difference = firstNumber > secondNumber ? firstNumber - secondNumber : secondNumber - firstNumber;
+                int product = firstNumber * secondNumber;
+                Console.WriteLine($"Sum: {sum}");
+                Console.WriteLine($"Difference: {difference}");
+                Console.WriteLine($"Product: {product}");
+                
+                if (firstNumber == 0 || secondNumber == 0) {
+                    Console.WriteLine("Error: Division by zero is not allowed!");
+                } else {
+                    int quotient = firstNumber > secondNumber ? firstNumber / secondNumber : secondNumber / firstNumber;
+                    Console.WriteLine($"quotient: {quotient}");
                 }
-                else
-                {
-                    Console.WriteLine("NO");
-                }
+                               
             }
+            else {
+
+                Console.WriteLine("Error: One or both inputs are not valid numbers!");
+            }
+
 
         }
     }
